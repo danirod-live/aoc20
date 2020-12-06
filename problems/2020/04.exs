4ff7c447bbf09a3e24248e0903e1346ec329b970
Dani Rodríguez <dani@danirod.es>
2020-12-06 18:30:44 +0100

    format 04

--- problems/2020/04.exs
+++ problems/2020/04.exs
@@ -20,7 +20,7 @@ defmodule Aoc2020.Problem04 do
   def problem1(data) do
     data
     |> Enum.filter(&valid1?/1)
-    |> Enum.count
+    |> Enum.count()
   end
 
   defp valid_data?(_, nil, _), do: false
@@ -55,24 +55,26 @@ defmodule Aoc2020.Problem04 do
   end
 
   defp valid2?(line) do
-    passport = line
-               |> String.split(" ")
-               |> Enum.map(&String.split(&1, ":"))
-               |> Enum.map(fn [k, v] -> {k, v} end)
-               |> Map.new
-    valid_data?(:range, passport["byr"], {1920, 2002})
-    && valid_data?(:range, passport["iyr"], {2010, 2020})
-    && valid_data?(:range, passport["eyr"], {2020, 2030})
-    && valid_data?(:height, passport["hgt"], {})
-    && valid_data?(:color, passport["hcl"], {})
-    && valid_data?(:eye, passport["ecl"], {})
-    && valid_data?(:passport, passport["pid"], {})
+    passport =
+      line
+      |> String.split(" ")
+      |> Enum.map(&String.split(&1, ":"))
+      |> Enum.map(fn [k, v] -> {k, v} end)
+      |> Map.new()
+
+    valid_data?(:range, passport["byr"], {1920, 2002}) &&
+      valid_data?(:range, passport["iyr"], {2010, 2020}) &&
+      valid_data?(:range, passport["eyr"], {2020, 2030}) &&
+      valid_data?(:height, passport["hgt"], {}) &&
+      valid_data?(:color, passport["hcl"], {}) &&
+      valid_data?(:eye, passport["ecl"], {}) &&
+      valid_data?(:passport, passport["pid"], {})
   end
 
   def problem2(data) do
     data
     |> Enum.filter(&valid2?/1)
-    |> Enum.count
+    |> Enum.count()
   end
 end
 
@@ -81,5 +83,5 @@ real_data = Aoc2020.Problem04.read_file("problems/2020/04.input")
 invalid_data = Aoc2020.Problem04.read_file("problems/2020/04.sample2")
 valid_data = Aoc2020.Problem04.read_file("problems/2020/04.sample3")
 
-AOC.runner &Aoc2020.Problem04.problem1/1, [sample_data, real_data]
-AOC.runner &Aoc2020.Problem04.problem2/1, [invalid_data, valid_data, real_data]
+AOC.runner(&Aoc2020.Problem04.problem1/1, [sample_data, real_data])
+AOC.runner(&Aoc2020.Problem04.problem2/1, [invalid_data, valid_data, real_data])
